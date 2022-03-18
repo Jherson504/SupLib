f6a72a5ee22d2929014315929586312b2be6da38
Jherson504 <jhersonclorado08@gmail.com>
2022-03-18 22:51:15 +0800

    user login page(unfinised)

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login_page, name='login'),
+    path('sign-in/', views.login_page, name='sign-in'),
+
 ]
--- base/views.py
+++ base/views.py
@@ -6,3 +6,9 @@ def home(request):
     __books = Book.objects.all()
     context = {'books': __books}
     return render(request, 'base/home.html', context)
+
+
+def login_page(request):
+    page = 'login'
+    context = {'page': page}
+    return render(request, 'base/login.html')
