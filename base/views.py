6ef9230d7be5d7166f49270392397d074c1529de
Jherson504 <jhersonclorado08@gmail.com>
2022-03-18 21:50:16 +0800

    home adjustments

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Book
 
 
-# home page
 def home(request):
-    return render(request, 'base/home.html')
+    __books = Book.objects.all()
+    context = {'books': __books}
+    return render(request, 'base/home.html', context)
